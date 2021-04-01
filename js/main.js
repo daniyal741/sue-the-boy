e514be22ef5e01215201b66492fcfbdb97f6db5f
daniyal741 <daniyalanwaar741@gmail.com>
2021-04-02 00:57:42 +0500

    Worked on Section#01

--- js/main.js
+++ js/main.js
@@ -5,8 +5,31 @@ $(document).ready(function(){
         
         $(".nav").toggle();
 
+        $(".nav").click(function(){
 
+        
+            $(".nav").hide();
+    
+    
+        });
     });
+
+   $(window).scroll(function(){
+
+    var scroll = $(window).scrollTop();
+
+    if(scroll >= 100)
+    {
+        $("header").addClass("scrollHeader");
+    }
+    else
+    {
+        $("header").removeClass("scrollHeader");
+
+    }
+
+   });
+
 
 
 });
